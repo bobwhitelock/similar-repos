fe798e86f2df04b4c64d3b209e4e7f81ff453aa7
Bob Whitelock <bob.whitelock1@gmail.com>
2016-10-18 22:29:33 +0100

    apollo-client issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, {createNetworkInterface} from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
 import './index.css';
 
+const client = ApolloClient({
+  networkInterface: createNetworkInterface('https://api.github.com/graphql')
+});
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
